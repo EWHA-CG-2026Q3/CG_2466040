858488f3d3b2757ba38284a0ef464596990b1cb9
Minkyoung Lee <minkyoungemilylee@ewha.ac.kr>
2026-09-10 10:10:17 +0900

    S04_커밋3_다이아몬드 만들어보기

--- Assets/S04/S04_My_Shape.cs
+++ Assets/S04/S04_My_Shape.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class S04_My_Shape : MonoBehaviour
@@ -15,17 +16,27 @@ public class S04_My_Shape : MonoBehaviour
             new Vector3(1f, 0f, 1f), // 5
             new Vector3(1f, 1f, 1f), // 6
             new Vector3(0f, 1f, 1f), // 7
-            new Vector3(0.5f, 1f, 0.5f) // 8
+            new Vector3(0.5f, 1f, 0.5f), // 8
+            new Vector3(0f, -1f, 0f), // 9
+            new Vector3(1f, -1f, 0f), //10
+            new Vector3(0f, -1f, 1f), // 11
+            new Vector3(1f, -1f, 1f), //12
+            new Vector3(0.5f, -1f, 0.5f), //13
         };
 
         int[] triangles = new int[]
         {
             0, 1, 4,
             5, 4, 1,
-            0, 1, 8,
-            1, 5, 8, 
+            0, 8, 1, 
+            1, 8, 5,
             5, 4, 8,
             4, 0, 8,
+            0, 1, 13,
+            1, 13, 5,
+            5, 13, 4,
+            0, 4, 13,
+            
         };
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
